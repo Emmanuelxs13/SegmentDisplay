52015958bc4d33d621a729951f456f99d2cf3272
Emmanuel <emmanuelberriojimenez13@gmail.com>
2024-09-28 22:53:01 -0500

    Segment Display Designer: -Se archivo para mejorar el manejo de las rutas

---
+++ Assets/Js/config.js
@@ -0,0 +1,7 @@
+// Assets/JS/config.js
+
+/**
+ * Archivo de configuración que define la ruta base del proyecto.
+ * '_DIR_' es el prefijo para todas las rutas relativas.
+ */
+const _DIR_ = 'Assets/';
